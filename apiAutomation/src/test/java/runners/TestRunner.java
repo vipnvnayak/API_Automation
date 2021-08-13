2329d77c244de36aab73afc979ea5435c5a00053
vipin <vipin@someone_pc>
2021-08-13 17:22:22 +0530

    Added Test Runner class

--- apiAutomation/src/test/java/runners/TestRunner.java
+++ apiAutomation/src/test/java/runners/TestRunner.java
@@ -1,5 +1,13 @@
 package runners;
 
-public class TestRunner {
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/regressionPack", glue = {
+		"stepDefinitions" }, monochrome = true, strict = true
+
+)
+public class TestRunner {
 }
